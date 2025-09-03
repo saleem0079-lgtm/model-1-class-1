06040fa63ba80d4e7d895a715e9c10b6f49a9a28
saleem0079-lgtm <saleem0079@gmail.com>
2025-09-03 18:51:47 -0500

    save

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print ("hello yasin how are you doing")
+print ("Hello",5)
+print ("Hello"*5)
+print ("Welcome to condingal",end="*")  
+print ()
+name="yasin"
+score=100
+print(score)
+print(f"{name} has scored {score} marks")
