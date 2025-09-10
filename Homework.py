f505b582590de114c3529597fb5f1d2c9216a45e
saleem0079-lgtm <saleem0079@gmail.com>
2025-09-10 18:45:50 -0500

    save

---
+++ Homework.py
@@ -0,0 +1,8 @@
+mybirthday="August 14" 
+print (f"my birthday is on {mybirthday}") 
+johnbirthday="Febuary 14"
+print (f"john birthday is on {johnbirthday}")
+jackbirthday="March 1"
+print (f"jack birthday is on {jackbirthday}")
+sarahbirthday="december 2"
+print (f"sarah birthday is on {sarahbirthday}")
